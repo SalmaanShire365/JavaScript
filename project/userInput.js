51b932ff6997190ccd31b5a56507ac3f4a7acce2
SalmaanShire365 <38257397+SalmaanShire365@users.noreply.github.com>
2022-05-24 20:17:51 -0500

    update files

--- project/userInput.js
+++ project/userInput.js
@@ -1,13 +1,13 @@
-// JavaScript source code
 
 const readline = require('readline');
 const { stdin: input, stdout: output } = require('process');
 
 const rl = readline.createInterface({ input, output });
 
-rl.question('Tell me your favorite color: ', (answer) => {
-    const isFavoriteColorBlue = answer == 'blue';
-    console.log(`Your favorite color is blue: ${isFavoriteColorBlue}`);
+rl.question('Do you know how to use userinput?: ', (answer) => {
+    // TODO: Log the answer in a database
+    const isAnswer = answer === 'yes';
+    console.log(`Come on, of course that's ${isAnswer}.`);
 
     rl.close();
 });

974ff489e6a4b153b1c3a3ae339bb001528c26e3
SalmaanShire365 <38257397+SalmaanShire365@users.noreply.github.com>
2022-05-24 20:23:37 -0500

    updates part 2

---
+++ project/whatType.js
@@ -0,0 +1,8 @@
+// write a program that prints out the type of a value
+
+const boolConst = true;
+const stringConst = 'some string';
+const numberConst = 55;
+console.log(`boolConst =  ${boolConst}, type ${typeof (boolConst)}`);
+console.log(`stringConst =  ${stringConst}, type ${typeof (stringConst)}`);
+console.log(`numberConst =  ${numberConst}, type ${typeof (numberConst)}`);

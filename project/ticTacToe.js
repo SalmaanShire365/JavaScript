b9c0b38880ad6f6b215d68abc92792778d9d9f86
SalmaanShire365 <salmaanshire@gmail.com>
2022-06-14 22:11:08 -0500

    Almost done learning Javascript, past Salmaan

--- project/ticTacToe.js
+++ project/ticTacToe.js
@@ -2,51 +2,51 @@ class TicTacToe {
 
     constructor() {
         this.board = [
-            [' ',' ',' ',],
-            [' ',' ',' ',],
-            [' ',' ',' ',],
+            [' ', ' ', ' ',],
+            [' ', ' ', ' ',],
+            [' ', ' ', ' ',],
             
 
         ];
     }
-    // getter for empty spaces on board
+
+        // getter for empty spaces on board
 
     get emptySpaces() {
-        this.board = board;
-        let spaces = 0;
-       // iterate over each row
-        for (let i = 0; i < board.length; i++) {
-            const row = board.length;
+            let board = this.board;
+            let spaces = 0;
+            // iterate over each row
+            for (let i = 0; i < board.length; i++) {
+                const row = board.length;
 
 
-            // iterate over each column in the row
-            for (let j = 0; j < row.length; i++) {
-                const columnVal = row[j];
+                // iterate over each column in the row
+                for (let j = 0; j < row.length; i++) {
+                    const columnVal = row[j];
 
 
-                // if the value is an empty space, count it
-                if (columnVal === ' ') spaces++;
-               
+                    // if the value is an empty space, count it
+                    if (columnVal === ' ') spaces++;
+
+                }
+
             }
-
+            return spaces;
         }
-        return spaces;
-    }
-
-   
 
 
-  // print out board so it isn't flat 
-    printBoard() {
-        const board = this.board;
-        console.log(`\nBoard below:`)
-        for (let i = 0; i < board.length; i++) {
-            console.log(board[i]);
+
+
+        // print out board so it isn't flat 
+        printBoard() {
+            const board = this.board;
+            console.log(`\nBoard below:`)
+            for (let i = 0; i < board.length; i++) {
+                console.log(board[i]);
+            }
         }
+
     }
-
-}
-
 
 const newGame = new TicTacToe();
 newGame.printBoard();

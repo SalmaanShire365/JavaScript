6da227405581be9750cf9657f83e2d4c3aa28ede
SalmaanShire365 <salmaanshire@gmail.com>
2022-07-05 15:14:52 -0500

    Update

---
+++ project/forEach.js
@@ -0,0 +1,8 @@
+// write a function that, given an array of integers (whole numbers), prints out
+// the cube of each integer in the array 
+
+
+
+[3, 6, 8, 17, ].forEach(currEl => {
+    console.log(currEl ** 3);
+});
